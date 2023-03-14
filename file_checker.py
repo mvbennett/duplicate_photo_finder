f36da125fbe9bafe0690cbeb536cf026471cb121
Mike Bennett <mvbennett9@gmail.com>
2023-03-14 03:26:14 -0400

    file checker finds duplicates of just image files sony users would use

--- file_checker.py
+++ file_checker.py
@@ -1,26 +1,33 @@
 import os
+import sys
+import re
 
-def main():
-    results = os.scandir('./images')
+def main(path):
+    results = os.scandir(path)
+    jpg = re.compile(r"\.jpg$", re.IGNORECASE)
+    dng = re.compile(r"\.dng$", re.IGNORECASE)
+    arw = re.compile(r"\.arw$", re.IGNORECASE)
     files = []
+    duplicates = []
     # print(results)
+    # test_file = results[0]
 
     def get_files(results):
-        files = []
         for file in results:
             if file.is_dir():
                 results = os.scandir(file.path)
-                files += get_files(results)
-            elif file.is_file():
+                get_files(results)
+            elif ({'name': file.name, 'size': file.stat().st_size} in files):
+                print(f"{file.name} is a duplicate!")
+                duplicates.append(file.path)
+            elif file.is_file() and (jpg.search(file.name) or dng.search(file.name) or arw.search(file.name)):
                 files.append({'name': file.name, 'size': file.stat().st_size})
 
-        return files
+    get_files(results)
 
-    files += get_files(results)
+    # print(files)
 
-    print(files)
-
-    # print(os.stat(f"./images/{file1}").st_size)
+    print(duplicates)
 
 if __name__ == "__main__":
-    main()
+    main(str(sys.argv[1]))
