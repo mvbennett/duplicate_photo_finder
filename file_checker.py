d75f18a6dde833f5906a813246bcda8becfb5987
Mike Bennett <mvbennett9@gmail.com>
2023-03-14 17:30:45 -0400

    can add directory names to skip on execution

--- file_checker.py
+++ file_checker.py
@@ -2,24 +2,42 @@ import os
 import sys
 import re
 
-def main(path):
-    results = os.scandir(path)
+def check_ext(filename):
     jpg = re.compile(r"\.jpg$", re.IGNORECASE)
     dng = re.compile(r"\.dng$", re.IGNORECASE)
     arw = re.compile(r"\.arw$", re.IGNORECASE)
+    tif = re.compile(r"\.tif$", re.IGNORECASE)
+    extensions = [jpg, dng, arw, tif]
+    for ext in extensions:
+        if ext.search(filename):
+            return True
+
+    return False
+
+def isnt_dirs(dirname, dirs = []):
+    for dir in dirs:
+        if dirname == dir:
+            return False
+
+    return True
+
+def main(path, skip_dirs = []):
+    if len(skip_dirs) >= 1:
+        print(f"Script will skip these directories: {skip_dirs}")
+    results = os.scandir(path)
     files = {}
     duplicates = []
 
     def get_files(results):
         for file in results:
-            if file.is_dir():
+            if file.is_dir() and isnt_dirs(file.name, skip_dirs):
                 results = os.scandir(file.path)
                 get_files(results)
             elif ({'name': file.name, 'size': file.stat().st_size} in files.values()):
                 original_path = list(files.keys())[list(files.values()).index({'name': file.name, 'size': file.stat().st_size})]
                 print(f"duplicate found: {original_path} --> {file.path}")
                 duplicates.append((original_path, file.path))
-            elif file.is_file() and (jpg.search(file.name) or dng.search(file.name) or arw.search(file.name)):
+            elif file.is_file() and check_ext(file.name):
                 files[file.path] = {'name': file.name, 'size': file.stat().st_size}
 
     get_files(results)
@@ -32,4 +50,11 @@ def main(path):
     result_file.close()
 
 if __name__ == "__main__":
-    main(str(sys.argv[1]))
+    if len(sys.argv) > 2:
+        skip_dirs = sys.argv[2:]
+        main(sys.argv[1], skip_dirs)
+    elif len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print(sys.argv[1:2])
+        main('.')
