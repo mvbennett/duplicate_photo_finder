bcaa48e6083fdd81f88c9d071f13eec2b19ef950
Mike Bennett <mvbennett9@gmail.com>
2023-03-14 12:17:38 -0400

    writes original and duplicate paths into file

--- file_checker.py
+++ file_checker.py
@@ -7,7 +7,7 @@ def main(path):
     jpg = re.compile(r"\.jpg$", re.IGNORECASE)
     dng = re.compile(r"\.dng$", re.IGNORECASE)
     arw = re.compile(r"\.arw$", re.IGNORECASE)
-    files = []
+    files = {}
     duplicates = []
 
     def get_files(results):
@@ -15,19 +15,21 @@ def main(path):
             if file.is_dir():
                 results = os.scandir(file.path)
                 get_files(results)
-            elif ({'name': file.name, 'size': file.stat().st_size} in files):
-                # print(f"{file.name} is a duplicate!")
-                duplicates.append(file.path)
+            elif ({'name': file.name, 'size': file.stat().st_size} in files.values()):
+                original_path = list(files.keys())[list(files.values()).index({'name': file.name, 'size': file.stat().st_size})]
+                print(f"duplicate found: {original_path} --> {file.path}")
+                duplicates.append((original_path, file.path))
             elif file.is_file() and (jpg.search(file.name) or dng.search(file.name) or arw.search(file.name)):
-                files.append({'name': file.name, 'size': file.stat().st_size})
+                files[file.path] = {'name': file.name, 'size': file.stat().st_size}
 
     get_files(results)
 
     result_file = open('./duplicates.txt', 'w+')
-    for file in duplicates:
-        result_file.write(f"{file}\n")
+    for original, duplicate in duplicates:
+        result_file.write(f"{original}  -->  {duplicate}\n")
+        # print(f"{original}  -->  {duplicate}")
 
-    # print(duplicates)
+    result_file.close()
 
 if __name__ == "__main__":
     main(str(sys.argv[1]))
